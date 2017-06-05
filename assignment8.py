012998db20f31ddc2c209bc21d2a9cde2afcd057
prachichikshe <pchikshe12@gmail.com>
2017-06-05 21:27:38 +0530

    Add files via upload

---
+++ assignment8.py
@@ -0,0 +1,15 @@
+# 8.Define a function max() that takes two numbers as arguments and returns the largest of them.
+def max(a, b):
+    #DocString
+    """ Function to find out the maximum of two numbers """
+    
+    if a > b:
+        return a
+    else:
+        return b
+
+A=input('Enter the first number')
+
+B=input('Enter the second number')
+
+print (str(max(A,B)) + ' is larger')
