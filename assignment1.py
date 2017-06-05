c12ebaf895966e51e3ed70ab535ccafe0d608e7c
prachichikshe <pchikshe12@gmail.com>
2017-06-05 18:27:19 +0530

    Delete assignment1.py

--- assignment1.py
+++
@@ -1,8 +0,0 @@
- Write a python script for creating directory,displaying its contents.
-import os
-os.mkdir('Assignment1')
-os.chdir('Assignment1')
-os.system('touch a.txt')
-os.system('touch a1.txt')
-print os.listdir(os.curdir)
-print os.listdir('--')
