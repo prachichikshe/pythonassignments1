26ef0c0da08dbb9a91e920daafa28a41d73ffe1a
prachichikshe <pchikshe12@gmail.com>
2017-06-05 22:26:10 +0530

    Update assignment10.py

--- assignment10.py
+++ assignment10.py
@@ -1,9 +1,9 @@
 #10.Define a function that computes the length of a given list or string.(prachi)
 def countStringLength(inputString):
  #DocString
-count = 0
+count=0
 for i in inputString:
-  count = count+1
+  count=count+1
 print ("Length of the String is " +str(count))
 print ("Function to find the length of the input String")
 s=input(print("Enter the string"))
