1f221bcb703e2a7ade295650bb613663c023a54f
prachichikshe <pchikshe12@gmail.com>
2017-06-05 21:51:38 +0530

    Update assignment9.py

--- assignment9.py
+++ assignment9.py
@@ -1,7 +1,6 @@
 #9.Define a function max_of_three() that takes three numbers as arguments and returns the largest of them.
 def max_of_three(a,b,c):
     #DocString
-    
     if a > b:
         if a > c:
             return a
