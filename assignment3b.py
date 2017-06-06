48afe29e1e1fd5ebcdd10eab59f9bac44f7f3675
prachichikshe <pchikshe12@gmail.com>
2017-06-06 15:27:38 +0530

    Create assignment3b.py
    assignment 3b

---
+++ assignment3b.py
@@ -0,0 +1,25 @@
+ #Q3 b. insert some values [eg. employee details]in db table.
+ 
+ import sqlite3
+
+# Open database connection
+db = sqlite3.connect("dbdemo.db" )
+
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
+
+# Prepare SQL query to INSERT a record into the database.
+sql = """INSERT INTO EMPLOYEE(FIRST_NAME,
+         LAST_NAME, AGE, SEX, INCOME)
+         VALUES ('Mac', 'Mohan', 20, 'M', 2000)"""
+try:
+   # Execute the SQL command
+   cursor.execute(sql)
+   # Commit your changes in the database
+   db.commit()
+except:
+   # Rollback in case there is any error
+   db.rollback()
+
+# disconnect from server
+db.close()
