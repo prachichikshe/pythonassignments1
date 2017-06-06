d52a8e06f7daa92a7cabc250e425dd13d71a6eb0
prachichikshe <pchikshe12@gmail.com>
2017-06-06 17:21:15 +0530

    Update assignment3.py

--- assignment3.py
+++ assignment3.py
@@ -1,44 +1,30 @@
 # Question 3:Write a python standalone program a. insert some values [eg. employee details]in db tab b. fetch the same data and print it on standard output.c. update specific employee info.d. delete specific employee and all the info. 
 
 import sqlite3 
-conn = sqlite3.connect('emp4.db') 
+conn = sqlite3.connect('demo.db') 
 print "Opened database successfully"; 
 # prepare a cursor object using cursor() method 
 cursor = conn.cursor()
 
 # Create table as per requirement 
-sql = """CREATE TABLE EMPLOYEE5 ( 
+sql = """CREATE TABLE EMPLOYEE1 ( 
          FIRST_NAME  CHAR(20) NOT NULL, 
          LAST_NAME  CHAR(20), 
          AGE INT,   
          SEX CHAR(1), 
          INCOME FLOAT )""" 
  
+ 
 cursor.execute(sql) 
 print "table create successfully" 
 # insert value in table 
-cursor.execute ("INSERT INTO EMPLOYEE5(FIRST_NAME, LAST_NAME, AGE, SEX ,INCOME) \ 
-     VALUES ('pragya', 'Gupta', 24, 'female', 20000.00 )"); 
- print "Records insertsuccessfully" 
+cursor.execute ("INSERT INTO EMPLOYEE1(FIRST_NAME, LAST_NAME, AGE, SEX ,INCOME) \ 
+     VALUES ('prachi', 'Chikshe', 24, 'female', 20000.00 )"); 
+ print "Records insert successfully" 
  
  
  #display data here 
- cursor.execute ("SELECT FIRST_NAME, LAST_NAME, AGE, SEX ,INCOME from EMPLOYEE5 ") 
- for row in cursor: 
-   print "FIRST_NAME = ", row[0] 
-   print "LAST_NAME = ", row[1] 
-   print "AGE = ", row[2] 
-   print "SEX = ", row[3] 
-   print "INCOME= ", row[4],"\n" 
- conn.commit() 
- print "Operation done successfully" 
- 
- 
- #update data 
- cursor.execute("UPDATE EMPLOYEE5 set INCOME=25000 where FIRST_NAME='pragya'") 
- conn.commit() 
- #display data here after updation 
- cursor.execute ("SELECT FIRST_NAME, LAST_NAME, AGE, SEX ,INCOME from EMPLOYEE5 ") 
+ cursor.execute ("SELECT FIRST_NAME, LAST_NAME, AGE, SEX ,INCOME from EMPLOYEE1 ") 
  for row in cursor: 
     print "FIRST_NAME = ", row[0] 
     print "LAST_NAME = ", row[1] 
@@ -46,26 +32,42 @@ cursor.execute ("INSERT INTO EMPLOYEE5(FIRST_NAME, LAST_NAME, AGE, SEX ,INCOME) 
     print "SEX = ", row[3] 
     print "INCOME= ", row[4],"\n" 
  conn.commit() 
- print "Records update successfully";
+ print "Operation done successfully" 
+ 
+ 
+ #update data 
+ cursor.execute("UPDATE EMPLOYEE1 set INCOME=25000 where FIRST_NAME='prachi'") 
+ conn.commit() 
+ #display data here after updation 
+cursor.execute ("SELECT FIRST_NAME, LAST_NAME, AGE, SEX ,INCOME from EMPLOYEE1 ") 
+ for row in cursor: 
+    print "FIRST_NAME = ", row[0] 
+    print "LAST_NAME = ", row[1] 
+    print "AGE = ", row[2] 
+    print "SEX = ", row[3] 
+    print "INCOME= ", row[4],"\n" 
+conn.commit() 
+print "Records update successfully";
 
 
  
  # insert value in table 
- cursor.execute ("INSERT INTO EMPLOYEE5(FIRST_NAME, LAST_NAME, AGE, SEX ,INCOME) \ 
+ cursor.execute ("INSERT INTO EMPLOYEE1(FIRST_NAME, LAST_NAME, AGE, SEX ,INCOME) \ 
        VALUES ('tina', 'agrawal', 22, 'female', 26000.00 )"); 
 print "Records insert successfully" 
  
  
  # delete 
-cursor.execute("DELETE from EMPLOYEE5 where LAST_NAME = 'Gupta'") 
+cursor.execute("DELETE from EMPLOYEE1 where LAST_NAME = 'Chikshe'") 
 conn.commit() 
  #display data here after updation 
-cursor.execute ("SELECT FIRST_NAME, LAST_NAME, AGE, SEX ,INCOME from EMPLOYEE5 ") 
+cursor.execute ("SELECT FIRST_NAME, LAST_NAME, AGE, SEX ,INCOME from EMPLOYEE1 ") 
  for row in cursor: 
-   print "FIRST_NAME = ", row[0] 
+    print "FIRST_NAME = ", row[0] 
     print "LAST_NAME = ", row[1] 
     print "AGE = ", row[2] 
     print "SEX = ", row[3] 
     print "INCOME= ", row[4],"\n" 
 print "Records distroy successfully"; 
 conn.close()
+
