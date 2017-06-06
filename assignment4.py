bb55a0899ec90dda71749b829deb6bed16229062
prachichikshe <pchikshe12@gmail.com>
2017-06-06 14:24:15 +0530

    Update assignment4.py

--- assignment4.py
+++ assignment4.py
@@ -1,10 +1,10 @@
 # Question4 :Write a Python program to count the number of lines in a text file
 
 import io
-fo=open("tina.txt","w+")
-fo.write("hello\nTina\nGupta\nZensar")
+fo=open("aditi.txt","w+")
+fo.write("hello\nAditi\nChikshe\nZensar")
 fo.close()
-filename="tina.txt"
+filename="aditi.txt"
 myfile = open(filename)
 lines= len(myfile.readlines())
 print "There are%d lines in %s "%(lines,filename)
@@ -12,6 +12,6 @@ print "There are%d lines in %s "%(lines,filename)
 
 '''
 output:
-ot@demo pragya_g16]# python forth.py
-There are4 lines in tina.txt
+
+There are4 lines in aditi.txt
 '''
