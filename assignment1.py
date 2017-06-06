628e19232cfbdebf27178e062695deb06a94cc05
prachichikshe <pchikshe12@gmail.com>
2017-06-06 13:03:08 +0530

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -1,4 +1,4 @@
-1. Write a python script for creating directory,displaying its contents.
+#1. Write a python script for creating directory,displaying its contents.
 
 import os
 os.mkdir('Assignment1')
@@ -6,5 +6,8 @@ os.chdir('Assignment1')
 os.system('touch a.txt')
 os.system('touch a1.txt')
 print os.listdir(os.curdir)
-print os.listdir('--')
+print os.listdir('..')
+
+"""Output
+python assignment1.py['a.txt', 'a1.txt']
 
