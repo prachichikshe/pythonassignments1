9670a1b8161a1bfef47b68f43bef6f9c34ca4cbb
prachichikshe <pchikshe12@gmail.com>
2017-06-06 16:07:25 +0530

    Create assignment3e.py

---
+++ assignment3e.py
@@ -0,0 +1,23 @@
+#Q 3e. delete specific employee and all the info.
+
+import sqlite3
+
+# Open database connection
+db = sqlite3.connect("dbdemo.db")
+
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
+
+# Prepare SQL query to DELETE required records
+sql = "DELETE FROM EMPLOYEE WHERE AGE > '%d'" % (20)
+try:
+   # Execute the SQL command
+   cursor.execute(sql)
+   # Commit your changes in the database
+   db.commit()
+except:
+   # Rollback in case there is any error
+   db.rollback()
+
+# disconnect from server
+db.close()
