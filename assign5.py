09a6892f522d4ea1252be931e889e6f605762e0a
prachichikshe <pchikshe12@gmail.com>
2017-06-06 16:28:31 +0530

    Update assign5.py

--- assign5.py
+++ assign5.py
@@ -1,5 +1,14 @@
+#Q5.Define a function that can accept two strings as input and concatenate them and then print it in console.
+
 string1=raw_input("Enter the string1")
 string2=raw_input("Enter the string2")
 string=(string1+string2)
 print(string)
+
+'''Output
+Enter the string1 prachi
+Enter the string2 chikshe
+prachi chikshe'''
+
+
 
