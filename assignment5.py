5964a6948037592575a703f270e4c880d295d88b
prachichikshe <pchikshe12@gmail.com>
2017-06-06 16:30:59 +0530

    Update assignment5.py

--- assignment5.py
+++ assignment5.py
@@ -1,3 +1,8 @@
+'''Q5.Define a function that can accept two strings as input and concatenate them and then print it in console.'''
+
 def concat(s1,s2):
 	print(s1+s2)
- concat("prachi","chikshe")
+concat("prachi","chikshe")
+
+'''Output
+prachi chikshe '''
